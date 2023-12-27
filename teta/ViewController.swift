6846914d110c8fe32b2368d8ab4bee62d44cb68b
Dinar Shakirov <dinarshakirovdev@gmail.com>
2023-12-27 12:17:01 +0700

    Initial Commit

---
+++ teta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teta
+//
+//  Created by Dinar Shakirov on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
