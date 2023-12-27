73f0c49d61a0dc186a7683f3b38cadf4dc40c687
Dinar Shakirov <dinarshakirovdev@gmail.com>
2023-12-27 12:38:37 +0700

    prepare for development

---
+++ teta/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  teta
+//
+//  Created by Dinar Shakirov on 27.12.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemPink
+    }
+    
+
+}
---
+++ teta/Support/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  teta
+//
+//  Created by Dinar Shakirov on 27.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        self.window?.windowScene = windowScene
+        
+        let homeVC = LoginViewController()
+        
+        let navController = UINavigationController(rootViewController: homeVC)
+        navController.navigationBar.topItem?.backButtonTitle = ""
+        self.window?.rootViewController = navController
+        self.window?.makeKeyAndVisible()
+    }
+
+}
+
